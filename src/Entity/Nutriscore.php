1da4bf055a44302e4575498abbceba8351d0e37a
Aissam-salman <nasawi.lmj@gmail.com>
2024-01-12 13:11:40 +0100

    creation entity Nutriscore

---
+++ src/Entity/Nutriscore.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: "nutriscore")]
+class Nutriscore
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue(strategy:"AUTO")]
+    #[ORM\Column(name: "nutriscore_id", type: "integer")]
+    private $id;
+
+    #[ORM\Column(name: "nutriscore_grade", type: "string", length: 2)]
+    private $grade;
+
+    // Getters and Setters
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getGrade(): ?string
+    {
+        return $this->grade;
+    }
+
+    public function setGrade(string $grade): self
+    {
+        $this->grade = $grade;
+        return $this;
+    }
+}
