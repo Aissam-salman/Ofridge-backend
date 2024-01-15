b74611e6e02d678656a217f8de3bf71b5446a145
Aissam-salman <nasawi.lmj@gmail.com>
2024-01-15 08:57:34 +0100

    add category entity

---
+++ src/Entity/Category.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: "category")]
+class Category {
+    #[ORM\Id]
+    #[ORM\GeneratedValue(strategy:"AUTO")]
+    #[ORM\Column(name: "category_id",type: "integer")]
+    private int $id;
+
+    #[ORM\Column(name: "category_name",type: "string", length: 50, nullable: false)]
+    private string $name;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $categoryId): self
+    {
+        $this->id = $categoryId;
+        return $this;
+    }
+
+    public function getName():?string
+    {
+        return $this->name;
+    }
+    public function setName(string $pName): self
+    {
+        $this->name = $pName;
+        return $this;
+    }
+
+}
