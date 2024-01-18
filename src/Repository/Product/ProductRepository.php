ad86e215fe019ee08a74164ccd14ce319b93115a
Aissam-salman <nasawi.lmj@gmail.com>
2024-01-18 13:38:57 +0100

    class is over maybe, ready to pass in service stack

--- src/Repository/Product/ProductRepository.php
+++ src/Repository/Product/ProductRepository.php
@@ -10,6 +10,7 @@ use App\ClientApi\OpenFactFoodapi;
 class ProductRepository extends ServiceEntityRepository
 {
     private OpenFactFoodapi $openFactFoodapi;
+
     public function __construct(ManagerRegistry $registry,OpenFactFoodapi $openFactFoodapi)
     {
         parent::__construct($registry, Product::class);
@@ -27,6 +28,37 @@ class ProductRepository extends ServiceEntityRepository
         } else {
             throw new \InvalidArgumentException ("No products found with the provided product name.");
         }
+    }
+    public function findProductsByProductCode(int $productCode): Product|array
+    {
+        if(!is_int($productCode) || preg_match('/^[0-9]+$/', $productCode) === 0) {
+            throw new \InvalidArgumentException("Product code must be an integer.");
+        }
+        $entityManager = $this->getEntityManager();
+        $products = $entityManager->find(Product::class, $productCode);
+        if ($products instanceof Product) {
+            return $products;
+        } else {
+            throw new \InvalidArgumentException ("No products found with the provided product code.");
+        }
+    }
+
+    public function apiFindProductsByKeyword(string $keyword): array
+    {
+        if (!is_string($keyword) || strlen(trim($keyword)) == 0) {
+            throw new \InvalidArgumentException("Product name must be a non-empty string.");
+        }
+
+        return $this->openFactFoodapi->getByKeyword($keyword);
+    }
+
+    public function apiFindProductsByBarcode(int $barcode): array
+    {
+        if (!is_int($barcode) || preg_match('/^[0-9]+$/', $barcode) === 0) {
+            throw new \InvalidArgumentException("Product code must be an integer.");
+        }
+
+        return $this->openFactFoodapi->getByBarcode($barcode);
     }
 }
 
