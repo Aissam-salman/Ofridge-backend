8dcc7b56c61f69db0a2d97e2ed77a195368ddb40
Aissam-salman <nasawi.lmj@gmail.com>
2024-01-22 10:37:18 +0100

    route /product/{product} finish

--- src/Repository/Product/ProductRepository.php
+++ src/Repository/Product/ProductRepository.php
@@ -5,14 +5,15 @@ use App\Entity\Product\Product;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use App\ClientApi\OpenFactFoodApi;
+use Symfony\Component\HttpClient\HttpClient;
 
 class ProductRepository extends ServiceEntityRepository
 {
     private OpenFactFoodApi $openFactFoodApi;
-    public function __construct(ManagerRegistry $registry,OpenFactFoodApi $openFactFoodApi)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Product::class);
-        $this->openFactFoodApi = $openFactFoodApi;
+        $this->openFactFoodApi = OpenFactFoodApi::getInstance(HttpClient::create());
     }
     public function findProductsByProductName(string $productName): Product|array
     {
@@ -24,7 +25,7 @@ class ProductRepository extends ServiceEntityRepository
         if ($products instanceof Product) {
             return $products;
         } else {
-            throw new \InvalidArgumentException ("No products found with the provided product name.");
+            throw new \InvalidArgumentException ("No products ici found with the provided product name.");
         }
     }
     public function findProductsByProductCode(int $productCode): Product|array
@@ -42,6 +43,7 @@ class ProductRepository extends ServiceEntityRepository
     }
     public function apiFindProductsByKeyword(string $keyword): array
     {
+
         if (!is_string($keyword) || strlen(trim($keyword)) == 0) {
             throw new \InvalidArgumentException("Product name must be a non-empty string.");
         }
@@ -53,6 +55,27 @@ class ProductRepository extends ServiceEntityRepository
             throw new \InvalidArgumentException("Product code must be an integer.");
         }
         return $this->openFactFoodApi->getByBarcode($barcode);
+    }
+
+    public function creaeProductFromApiResponse(array $response): Product
+    {
+        $product = new Product();
+        $product->setId($response['_id']);
+        $product->setName($response['product_name']);
+        $product->setAllergens($response['allergens_imported']);
+        $product->setBrand($response['brands']);
+        $product->setGenericName($response['generic_name']);
+        $product->setImgFront($response['selected_images']['front']);
+        $product->setPackaging($response['packaging_text']);
+        $product->setQuantity($response['quantity']);
+        $product->setNutriscore($response['nutriscore_2023_tags']);
+        $product->setCategory($response['categories_tags']);
+        $product->setCountries($response['countries']);
+        $product->setKeywords($response['_keywords']);
+        $product->setNutriments($response['nutriments']);
+        $product->setComposition($response['ingredients_text']);
+
+        return $product;
     }
 }
 
